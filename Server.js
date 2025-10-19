70be11880f94c59690f731af4c70c93d244dc5a6
Harsh Singh <your-email@example.com>
2025-10-20 00:47:33 +0530

    Initial commit

---
+++ Server.js
@@ -0,0 +1,17 @@
+const express= require('express');
+const app=express();
+const db=require('./db');
+require('dotenv').config;
+
+const bodyParser=require('body-parser');
+app.use(bodyParser.json());
+
+const PORT = process.env.PORT||3000;
+
+const userRoutes=require('./Routes/userRoutes');
+
+app.use('/user',userRoutes);
+
+app.listen(PORT,()=>{
+    console.log(`Server Running on Port ${PORT}`);
+})
